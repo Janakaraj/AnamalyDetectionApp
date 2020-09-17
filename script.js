20769b57fb8944f2ad48652231cc56befe479926
Janakaraj <janak@promactinfo.com>
2020-09-17 18:50:27 +0530

    Bug fixes

--- script.js
+++ script.js
@@ -5,16 +5,17 @@ const enableWebcamButton = document.getElementById('webcamButton');
 const stopButton = document.getElementById('stopButton');
 const results = document.getElementById('results');
 var reqId;
-
+var startTime;
+var endTime;
+var totalTime;
 
 var mobileDeviceTime = [];
-var personPresentTimeList=[];
-var personNotPresentTimeList = [];
+var personPresentTimeList = [];
 var personPartiallyPresentTimeList = [];
 var faceCoveredTimeList = [];
-var faceDetectedTimeList=[];
-var multipleUserTimeList=[];
-var faceCoverCounter =0;
+var faceDetectedTimeList = [];
+var multipleUserTimeList = [];
+var faceCoverCounter = 0;
 function getUserMediaSupported() {
   return !!(navigator.mediaDevices &&
     navigator.mediaDevices.getUserMedia);
@@ -45,6 +46,8 @@ function enableCam(event) {
     video.srcObject = stream;
     video.addEventListener('loadeddata', predictWebcam);
   });
+  var startDate = new Date();
+  startTime = startDate.getTime();
 }
 // script tag import so ignore any warning in Glitch.
 blazeface.load().then(function (loadedmodel) {
@@ -83,17 +86,14 @@ function predictWebcam() {
           var noo = d.getTime();
           mobileDeviceTime.push(noo);
         }
-        if (predictions[n].class != "person") {
-          var d = new Date();
-          var noo = d.getTime();
-          personNotPresentTimeList.push(noo);
-        }
+
         if (predictions[n].class == "person") {
           var d = new Date();
           var noo = d.getTime();
           personPresentTimeList.push(noo);
         }
-        if (predictions[n].class == "person" && predictions[n].score <= 0.80) {
+
+        if (predictions[n].class == "person" && predictions[n].score <= 0.75) {
           var d = new Date();
           var noo = d.getTime();
           personPartiallyPresentTimeList.push(noo);
@@ -125,37 +125,35 @@ function predictWebcam() {
   detectFace();
   stopButton.addEventListener('click', stopCam);
 }
-async function detectFace(){
+async function detectFace() {
   const predictions = await fdmodel.estimateFaces(video, false);
   for (let i = 0; i < predictions.length; i++) {
     var probability = predictions[i].probability;
-    if(probability <=90){
+    if (probability <= 90) {
       var d = new Date();
       var noo = d.getTime();
       faceDetectedTimeList.push(noo);
     }
   }
-  console.log(faceDetectedTimeList);
 }
-function findCommonTime(arr1, arr2) { 
-  for(let i = 0; i < arr1.length; i++) { 
-          
-    for(let j = 0; j < arr2.length; j++) { 
-          
-        if(arr1[i]>= arr2[j]-1000 && arr1[i] <= (arr2[j] + 1000) ) { 
-          faceCoveredTimeList.push(arr1[i]);
-        } 
-    } 
-    console.log(faceCoveredTimeList);
+function findCommonTime(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+
+    for (let j = 0; j < arr2.length; j++) {
+
+      if (arr1[i] >= arr2[j] - 1000 && arr1[i] <= (arr2[j] + 1000)) {
+        faceCoveredTimeList.push(arr1[i]);
+      }
+    }
   }
   faceCoveredCounter(faceCoveredTimeList);
 }
-function faceCoveredCounter(list){
+function faceCoveredCounter(list) {
   var forward = list[0];
   var tempI = list[0];
   for (var i = 0; i < list.length; i++) {
     if (list[i + 1] - list[i] <= 1000) {
-      forward = list[i + 1]
+      forward = list[i + 1];
     }
     else {
       faceCoverCounter++;
@@ -164,13 +162,13 @@ function faceCoveredCounter(list){
     }
   }
 }
-function getDuplicateArrayElements(arr){
+function getDuplicateArrayElements(arr) {
   var sorted_arr = arr.slice().sort();
   var results = [];
   for (var i = 0; i < sorted_arr.length - 1; i++) {
-      if (sorted_arr[i + 1] === sorted_arr[i]) {
-          results.push(sorted_arr[i]);
-      }
+    if (sorted_arr[i + 1] === sorted_arr[i]) {
+      results.push(sorted_arr[i]);
+    }
   }
   return results;
 }
@@ -180,12 +178,16 @@ function stopCam() {
   video.srcObject.getTracks().forEach(function (track) {
     track.stop();
   });
+  var endDate = new Date();
+  endTime = endDate.getTime();
+  totalTime = (endTime - startTime) / 1000;
   var cellPhoneDetectedFor = calulateTotalTime.apply(null, mobileDeviceTime);
-  var userNotPresentFor = calulateTotalTime.apply(null, personNotPresentTimeList);
+  var userNotPresentFor = parseFloat(totalTime - (calulateTotalTime.apply(null, personPresentTimeList)));
   var userPartiallyPresentFor = calulateTotalTime.apply(null, personPartiallyPresentTimeList);
   findCommonTime(faceDetectedTimeList, personPresentTimeList);
   multipleUserTimeList = getDuplicateArrayElements(personPresentTimeList);
   var multipleUserPresentFor = calulateTotalTime.apply(null, multipleUserTimeList);
+  console.log(personPartiallyPresentTimeList);
   addResultToHtml(cellPhoneDetectedFor, userNotPresentFor, userPartiallyPresentFor, multipleUserPresentFor, faceCoverCounter);
 }
 function calulateTotalTime() {
@@ -198,33 +200,43 @@ function calulateTotalTime() {
   var totalTime = 0;
   var tempDiff = 0;
   for (var i = 0; i < list.length; i++) {
-    if (list[i + 1] - list[i] <= 1000) {
-      forward = list[i + 1]
-    }
-    else {
-      tempDiff = forward - tempI;
-      totalTime = totalTime + tempDiff;
-      forward = list[i + 1];
-      tempI = list[i + 1];
+    if (list[i + 1] != list[i]) {
+      if (list[i + 1] - list[i] <= 1000) {
+        forward = list[i + 1]
+      }
+      else {
+        tempDiff = forward - tempI;
+        totalTime = totalTime + tempDiff;
+        forward = list[i + 1];
+        tempI = list[i + 1];
+      }
     }
   }
   return totalTime / 1000;
 }
-function addResultToHtml(a, b, c, d,e) {
+function addResultToHtml(a, b, c, d, e) {
+  var formate = "seconds";
+  if (totalTime > 300) {
+    a = a / 60;
+    b = b / 60;
+    c = c / 60;
+    d = d / 60;
+    formate = "minutes";
+  }
   var li = document.createElement('li');
   li.innerText = "Results";
   results.appendChild(li);
   var li = document.createElement('li');
-  li.innerText = "CellPhone and other electronic devices were detected for " + a + " seconds";
+  li.innerText = "CellPhone and other electronic devices were detected for " + a + " " + formate;
   results.appendChild(li);
   var li = document.createElement('li');
-  li.innerText = "User was not present for " + b + " seconds out of the total test time";
+  li.innerText = "User was not present for " + b + " " + formate + " out of " + totalTime + " " + formate + " of the total test time";
   results.appendChild(li);
   var li = document.createElement('li');
-  li.innerText = "User was partially present for " + c + " seconds out of the total test time";
+  li.innerText = "User was partially present for " + c + " " + formate + " out of " + totalTime + " " + formate + " of the total test time";
   results.appendChild(li);
   var li = document.createElement('li');
-  li.innerText = "Multiple users detected for " + d + " seconds out of the total test time";
+  li.innerText = "Multiple users detected for " + d + " " + formate + " out of " + totalTime + " " + formate + " of the total test time";
   results.appendChild(li);
   var li = document.createElement('li');
   li.innerText = "User covered his face " + e + " times";
